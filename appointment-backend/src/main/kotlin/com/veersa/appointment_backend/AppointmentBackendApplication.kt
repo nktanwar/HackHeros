833b6f887bccfb1d97fa73078a7e7eb144df3fb7
nk <nktanwar302@gmail.com>
2025-12-24 11:15:10 +0530

    Initial commit

---
+++ appointment-backend/src/main/kotlin/com/veersa/appointment_backend/AppointmentBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.veersa.appointment_backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AppointmentBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<AppointmentBackendApplication>(*args)
+}
